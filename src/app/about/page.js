2d5c26dd69a574d701ed7e999433c7270769d727
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-07-31 19:58:00 +0600

    updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -68,7 +68,50 @@ const Page = () => {
           watching!" Please note that the above description is fictional and
           created for illustrative purposes. For a real movie website, you would
           need to tailor the content to reflect the specific offerings and
-          values of your website.
+          values of your website. Whether you're a fan of thought-provoking
+          documentaries or in the mood for a good laugh with comedies, CineHub
+          has you covered. Exclusive Original Content: In addition to mainstream
+          films, CineHub is proud to present exclusive original content.
+          Discover thought-provoking indie films and breakthrough creations from
+          talented emerging filmmakers. We strive to support independent cinema
+          and provide a platform for innovative storytelling. Seamless
+          Streaming: Say goodbye to buffering and interruptions. CineHub offers
+          seamless streaming with high-definition picture quality and
+          crystal-clear sound. Watch your favorite movies without any
+          disruptions, immersing yourself fully in the cinematic experience.
+          Community and Interaction: At CineHub, we believe that movies are
+          meant to be shared and discussed. Join our vibrant community of movie
+          enthusiasts, where you can engage in lively discussions, share your
+          reviews, and interact with fellow cinephiles. Connect with like-minded
+          individuals who share your passion for cinema. Behind-the-Scenes
+          Insights: Unravel the magic of movie-making with our behind-the-scenes
+          insights. Explore exclusive interviews with filmmakers, actors, and
+          industry experts. Gain a deeper understanding of the creative process
+          and the art of storytelling through our in-depth features. Stay
+          Updated with the Latest News: Stay up-to-date with the latest
+          happenings in the movie world with our comprehensive news section.
+          From film festivals and award ceremonies to exciting casting
+          announcements, we keep you informed on all the industry buzz.
+          Family-Friendly Content: CineHub is a family-friendly platform where
+          you can enjoy a wide selection of films suitable for all ages. We
+          believe in creating a safe and inclusive environment for everyone to
+          appreciate the magic of cinema together. Accessibility and
+          Inclusivity: At CineHub, we are committed to ensuring our platform is
+          accessible and inclusive to all. We offer closed captioning and
+          multiple language options to cater to diverse audiences worldwide.
+          Supporting the Film Community: By subscribing to CineHub, you are not
+          only gaining access to an extraordinary movie-watching experience but
+          also contributing to the film community. A portion of your
+          subscription goes towards supporting aspiring filmmakers and promoting
+          cultural diversity in cinema. Experience the Enchantment of Cinema:
+          Whether you're a seasoned film buff or just starting your cinematic
+          journey, CineHub invites you to experience the enchantment of cinema
+          like never before. Join us on this cinematic adventure and let the
+          magic of movies transport you to extraordinary worlds. Thank you for
+          being a part of the CineHub family. Happy movie watching!" Please note
+          that the above description is fictional and created for illustrative
+          purposes. For a real movie website, you would need to tailor the
+          content to reflect the specific offerings and values of your website.
         </p>
       </div>
     </main>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,34 @@
 import React from "react";
+import ContactCss from "./Contact.module.css";
 
 const Page = () => {
   return (
-    <div>
-      <h1>Hello from contact</h1>
+    <div className={ContactCss.contact_div}>
+      <form>
+        <div className={ContactCss.single_inputs}>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className={ContactCss.single_inputs}>
+          <label htmlFor="email1">Email:</label>
+          <input type="email" id="email1" name="email1" required />
+        </div>
+        <div className={ContactCss.single_inputs}>
+          <label htmlFor="email2">Confirm Email:</label>
+          <input type="email" id="email2" name="email2" required />
+        </div>
+        <div className={ContactCss.single_inputs}>
+          <label htmlFor="password1">Password:</label>
+          <input type="password" id="password1" name="password1" required />
+        </div>
+        <div className={ContactCss.single_inputs}>
+          <label htmlFor="password2">Confirm Password:</label>
+          <input type="password" id="password2" name="password2" required />
+        </div>
+        <button className={ContactCss.Submit_button} type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
